67b74edc604ba94e348d2f00460b452c9b7f2235
wallsieiswallsie <ralazinawla@gmail.com>
2026-04-15 18:39:10 +0700

    fix path

--- src/services/api/authApi.js
+++ src/services/api/authApi.js
@@ -3,19 +3,19 @@ import { apiFetch } from "./apiClient";
 /** endpoints module */
 export const authApi = {
   requestOtp: async (whatsapp_number) =>
-    apiFetch("/auth/otp", {
+    apiFetch("/otp", {
       method: "POST",
       body: JSON.stringify({ whatsapp_number }),
     }),
 
   register: async (payload) =>
-    apiFetch("/auth/users", {
+    apiFetch("/users", {
       method: "POST",
       body: JSON.stringify(payload),
     }),
 
   login: async (identifier, password) =>
-    apiFetch("/auth/login", {
+    apiFetch("/login", {
       method: "POST",
       body: JSON.stringify({ identifier, password }),
     }),
